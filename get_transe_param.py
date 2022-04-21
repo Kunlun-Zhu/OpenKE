570a203df8907b17324999a444a565ada8b144c1
Kunlun Zhu <zhuklun@mail2.sysu.edu.cn>
2022-04-21 18:09:20 +0800

    Create get_transe_param.py

---
+++ get_transe_param.py
@@ -0,0 +1,43 @@
+import openke
+from openke.config import Trainer, Tester
+from openke.module.model import TransE, TransR
+from openke.module.loss import MarginLoss
+from openke.module.strategy import NegativeSampling
+from openke.data import TrainDataLoader, TestDataLoader
+
+# dataloader for training
+train_dataloader = TrainDataLoader(
+	in_path = "./benchmarks/FB15K/", 
+	nbatches = 100,
+	threads = 8, 
+	sampling_mode = "normal", 
+	bern_flag = 1, 
+	filter_flag = 1, 
+	neg_ent = 25,
+	neg_rel = 0)
+
+# dataloader for test
+test_dataloader = TestDataLoader(
+	in_path = "./benchmarks/FB15K/",
+	sampling_mode = 'link')
+
+# define the model
+transe = TransE(
+	ent_tot = train_dataloader.get_ent_tot(),
+	rel_tot = train_dataloader.get_rel_tot(),
+	dim = 256, 
+	p_norm = 1, 
+	norm_flag = True)
+
+model_e = NegativeSampling(
+	model = transe, 
+	loss = MarginLoss(margin = 5.0),
+	batch_size = train_dataloader.get_batch_size())
+
+
+# pretrain transe
+trainer = Trainer(model = model_e, data_loader = train_dataloader, train_times = 1, alpha = 0.5, use_gpu = True)
+trainer.run()
+parameters = transe.get_parameters()
+transe.save_parameters("./result/transe_256.json")
+print("saved_done")
